c0535b608ed8892c45c5ac3b4fc78d6732b35851
Adela_Gorea <adelaosadta@gmail.com>
2020-07-29 20:10:13 +0300

    Ex.2 si 3

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -1,4 +1,16 @@
-a = 10
-a += len(str(a))
-a = str(a)
-print(a)
+# Ex.1
+# a = 10
+# a += len(str(a))
+# a = str(a)
+# print(a)
+# Va afisa valoarea 12
+# Ex.2
+#print  ("In padurea cu alune aveau casa" + " 2 " + "pitici")
+#Ex.3
+# a = 10
+# print(a + 1)
+# Va afisa valoarea 12
+#Ex.4
+a = input()
+a += 1
+
