6bc9f5f57f2034f3dae073ff386a72e392e909da
Adela_Gorea <adelaosadta@gmail.com>
2020-07-29 20:50:30 +0300

    Ex.6

---
+++ lesson2/Ex6.py
@@ -0,0 +1,16 @@
+
+a = ['a', 'b', 'c', 'd']
+#print(a[int(int('3' * 2) // 11)])
+#va afisa 'd'
+   
+# a.insert(0, 'g')
+# print(a)
+
+# del a[-2:]
+# print(a)
+
+# a.sort(reverse=True)   
+# print(a)
+
+b = list(a[2:])
+print(b)
