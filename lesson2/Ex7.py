23b8ecc92d05fc7f24e62a6561cb9a99cf1bbda9
Adela_Gorea <adelaosadta@gmail.com>
2020-07-29 20:51:48 +0300

    Ex.7

---
+++ lesson2/Ex7.py
@@ -0,0 +1,11 @@
+inventar =	{
+  "scaune": 23,
+  "mese": 48,
+  "dulapuri": 19,
+  "cuer":21
+}
+
+sum1 = sum(inventar[item] for item in inventar)
+print(sum1)
+
+# sum(inventar(values()) -  nu sa primit
